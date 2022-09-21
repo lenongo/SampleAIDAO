84d28f05f77e449f5a9c1a54c97656f95fb6ad7c
shun <funakki0904@gmail.com>
2022-09-21 23:05:27 +0900

    "価格などの変更"

--- src/App.js
+++ src/App.js
@@ -101,6 +101,7 @@ function App() {
   const [claimingNft, setClaimingNft] = useState(false);
   const [feedback, setFeedback] = useState(`Click buy to mint your NFT.`);
   const [mintAmount, setMintAmount] = useState(1);
+  const [isWL, setIsWL] = useState(false);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
     SCAN_LINK: "",
@@ -125,12 +126,56 @@ function App() {
     let gasLimit = CONFIG.GAS_LIMIT;
     let totalCostWei = String(cost * mintAmount);
     let totalGasLimit = String(gasLimit * mintAmount);
+    let walletAddress = String(blockchain.account);
+
     console.log("Cost: ", totalCostWei);
     console.log("Gas limit: ", totalGasLimit);
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .mint(mintAmount)
+      .mint(
+        mintAmount,
+        walletAddress,
+        "0xda8bf60a45363f6f8f79b48d71ee376f5e5265b3"
+      )
+      .send({
+        gasLimit: String(totalGasLimit),
+        to: CONFIG.CONTRACT_ADDRESS,
+        from: blockchain.account,
+        value: totalCostWei,
+      })
+      .once("error", (err) => {
+        console.log(err);
+        setFeedback("Sorry, something went wrong please try again later.");
+        setClaimingNft(false);
+      })
+      .then((receipt) => {
+        console.log(receipt);
+        setFeedback(
+          `WOW, the ${CONFIG.NFT_NAME} is yours! go visit Opensea.io to view it.`
+        );
+        setClaimingNft(false);
+        dispatch(fetchData(blockchain.account));
+      });
+  };
+
+  const claimWLNFTs = () => {
+    let cost = CONFIG.WL_WEI_COST;
+    let gasLimit = CONFIG.GAS_LIMIT;
+    let totalCostWei = String(cost * mintAmount);
+    let totalGasLimit = String(gasLimit * mintAmount);
+    let walletAddress = String(blockchain.account);
+
+    console.log("Cost: ", totalCostWei);
+    console.log("Gas limit: ", totalGasLimit);
+    setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
+    setClaimingNft(true);
+    blockchain.smartContract.methods
+      .WLmint(
+        mintAmount,
+        walletAddress,
+        "0xda8bf60a45363f6f8f79b48d71ee376f5e5265b3"
+      )
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
@@ -185,12 +230,21 @@ function App() {
     SET_CONFIG(config);
   };
 
+  //WLの認識
+  const getWL = () => {
+    if (blockchain.account === 0x2ad2d903fc5f0f28baa62ad5a377c05a2c07bab9) {
+      setIsWL(true);
+    }
+    console.log(isWL);
+  };
+
   useEffect(() => {
     getConfig();
   }, []);
 
   useEffect(() => {
     getData();
+    getWL();
   }, [blockchain.account]);
 
   return (
@@ -199,9 +253,9 @@ function App() {
         flex={1}
         ai={"center"}
         style={{ padding: 24, backgroundColor: "var(--primary)" }}
-        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
+        //image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
-        <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
+        {/* <StyledLogo alt={"logo"} /src={"/config/images/logo.png"} /> */}
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
@@ -263,8 +317,11 @@ function App() {
                 <s.TextTitle
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
                 >
-                  1 {CONFIG.SYMBOL} costs {CONFIG.DISPLAY_COST}{" "}
-                  {CONFIG.NETWORK.SYMBOL}.
+                  1 {CONFIG.SYMBOL} costs {CONFIG.WL_DISPLAY_COST}
+                  {CONFIG.NETWORK.SYMBOL}
+                  {"(WL)"}.<br />
+                  {CONFIG.DISPLAY_COST} {CONFIG.NETWORK.SYMBOL}
+                  {"(Public)"}.
                 </s.TextTitle>
                 <s.SpacerXSmall />
                 <s.TextDescription
@@ -352,16 +409,33 @@ function App() {
                     </s.Container>
                     <s.SpacerSmall />
                     <s.Container ai={"center"} jc={"center"} fd={"row"}>
-                      <StyledButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          claimNFTs();
-                          getData();
-                        }}
-                      >
-                        {claimingNft ? "BUSY" : "BUY"}
-                      </StyledButton>
+                      {isWL ? (
+                        <>
+                          <StyledButton
+                            disabled={claimingNft ? 1 : 0}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              claimWLNFTs();
+                              getData();
+                            }}
+                          >
+                            {claimingNft ? "BUSY" : "WL BUY"}
+                          </StyledButton>
+                        </>
+                      ) : (
+                        <>
+                          <StyledButton
+                            disabled={claimingNft ? 1 : 0}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              claimNFTs();
+                              getData();
+                            }}
+                          >
+                            {claimingNft ? "BUSY" : "BUY"}
+                          </StyledButton>
+                        </>
+                      )}
                     </s.Container>
                   </>
                 )}
